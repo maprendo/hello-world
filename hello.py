7b2e361538fd3ecaa48d2c4a093307cff1e26c0b
Matthias Patzak <patzak@ist-chemnitz.de>
2019-02-14 16:49:15 +0100

    Line uncommented.

--- hello.py
+++ hello.py
@@ -3,4 +3,5 @@ import datetime
 now = datetime.datetime.now()
 
 print('\nHello World. Today is %d-%d %d !\n' % (now.month, now.day,now.year))
-print('Awesome!\n')
+# The following line is not required.
+# print('Awesome!\n')
